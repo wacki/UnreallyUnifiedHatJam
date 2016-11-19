fac22bde85bac3b24b7aa73d86e9fd6191269bb9
Rodolfo <rdo_cnt@hotmail.com>
2016-11-18 23:44:37 -0800

    Terrain start

---
+++ Assets/GameHam/Scripts/StageObjects/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+namespace UU.GameHam
+{
+	public class MovingPlatform : MonoBehaviour
+	{
+
+
+		public float duration;
+		public GameObject end;
+		public Ease easings = Ease.InOutSine;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+			end.SetActive (false);
+			//Tweener object is used to animate a value, in this case it's the position of the transform
+			Tweener moveTween = transform.DOMove (end.transform.position, duration);
+			moveTween.SetLoops (-1, LoopType.Yoyo);
+
+			//This makes the movement that we provide relative to the start position
+			moveTween.SetRelative(true);
+
+			//Smoothes out the parabola effect into a sin effect. Look for easings.net for more
+			moveTween.SetEase (easings);
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	}
+}
---
+++ Assets/GameHam/Scripts/StageObjects/TerrainVisibility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UU.GameHam
+{
+	public class TerrainVisibility : MonoBehaviour
+	{
+		
+		public bool visible = false;
+		// Use this for initialization
+		void Start ()
+		{
+			gameObject.GetComponent<SpriteRenderer> ().enabled = visible;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	}
+}
